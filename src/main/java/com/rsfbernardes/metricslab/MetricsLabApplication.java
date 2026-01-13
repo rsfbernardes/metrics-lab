6df33f67ad728abf7fd1f0b36ce7b489a28d2b60
¨Rodrigo <rsfbernardes@hotmail.com>
2026-01-13 18:49:22 -0300

    Initial Spring Boot project with Actuator

---
+++ src/main/java/com/rsfbernardes/metricslab/MetricsLabApplication.java
@@ -0,0 +1,13 @@
+package com.rsfbernardes.metricslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsLabApplication.class, args);
+    }
+
+}
